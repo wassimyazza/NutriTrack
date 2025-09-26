2c20937a8b2ee03d71a4d8c1123127ffd6dde1c6
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-09-26 17:21:07 +0100

    feat(routes): add GET recommendations route

--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,7 @@
 // routes/web.js
 import express from 'express';
 import BookController from '../app/controllers/BookController.js';
+import RecommendationController from '../app/controllers/RecommendationController.js';
 
 const router = express.Router();
 
@@ -9,5 +10,7 @@ router.get('/books/:id', BookController.show);
 router.post('/books', BookController.store);
 router.post('/books/:id/update', BookController.update);
 router.post('/books/:id/delete', BookController.destroy);
+
+router.get('/recommendations', RecommendationController.index);
 
 export default router;
