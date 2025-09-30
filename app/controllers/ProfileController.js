c6df1c509ec3a43cf99251c56fb370ad0c383158
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-09-30 20:50:59 +0100

    feat(views):  display the loggedin user data in the profile form

--- app/controllers/ProfileController.js
+++ app/controllers/ProfileController.js
@@ -1,6 +1,10 @@
+import User from '../models/User.js';
+
 export default class ProfileController {
-   static show(req, res) {
-      res.render('profile/index');
+   static async show(req, res) {
+      const user_id = req.session.user.id;
+      const user = await User.find(user_id);
+      res.render('profile/index', {user});
    }
 
    static update(res, req) {}
