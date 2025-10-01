11baeffc445e093c2f7ac7b8e5b20ae25bb2f165
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-10-01 16:00:42 +0100

    feat(controllers): validate data before updating the user profile and provide errors to the view

--- app/controllers/ProfileController.js
+++ app/controllers/ProfileController.js
@@ -1,22 +1,31 @@
 import User from '../models/User.js';
 import bcrypt from 'bcrypt';
+import {validationResult} from 'express-validator';
 
 export default class ProfileController {
    static async show(req, res) {
       const user_id = req.session.user.id;
       const user = await User.find(user_id);
-      res.render('profile/index', {user});
+      res.render('profile/index', {user, errors: {}});
    }
 
    static async update(req, res) {
       const user_id = req.session.user.id;
-
       if (req.body.password.trim())
          req.body.password = await bcrypt.hash(req.body.password, 12);
 
-      const newUser = await User.update(user_id, req.body);
-      console.log(newUser);
+      const errors = validationResult(req).array();
+      const mappedErrors = {};
+      if (errors.length) {
+         errors.forEach(err => {
+            mappedErrors[err.path] = err.msg;
+         });
+      } else {
+         User.update(user_id, req.body);
+      }
 
-      res.render('profile/index', {user: newUser});
+      const newUser = await User.find(user_id);
+
+      res.render('profile/index', {user: newUser, errors: mappedErrors});
    }
 }
