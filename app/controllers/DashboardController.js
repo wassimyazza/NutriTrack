5838cef9a6dec88bbedd11b75e6c2b33e0c2c609
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-10-02 20:07:08 +0100

    feat(controllers): provide user informations to the view

--- app/controllers/DashboardController.js
+++ app/controllers/DashboardController.js
@@ -1,3 +1,10 @@
+import User from '../models/User.js';
 export default class DashboardController {
-   static async show(req, res) {}
+   static async show(req, res) {
+      const userId = req.session.user.id;
+      const user = await User.find(userId);
+      res.render('dashboard/index.ejs', {
+         user,
+      });
+   }
 }
